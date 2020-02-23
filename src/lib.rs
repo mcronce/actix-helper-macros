a4f0fd2c97e5611147f632b9894f0e24a4f17c3d
Mike Cronce <mike@cronce.io>
2020-02-23 15:45:23 -0500

    src/lib.rs:  + Response enum (THIS NOW COMPILES)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+extern crate actix_web;
+extern crate serde;
+
 extern crate actix_proc_macros;
 pub use actix_proc_macros::*;
+
+pub enum Response<T: serde::Serialize> {
+	Json(T),
+	Text(String),
+	Builder(actix_web::dev::HttpResponseBuilder)
+}
 
